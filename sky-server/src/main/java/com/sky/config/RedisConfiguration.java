f471abbd24ed505da5ce4f0c46e3c1e18e4cdedc
Sunx <1060448742@qq.com>
2024-12-31 10:40:35 +0800

    v4.0 店铺营业状态业务代码开发

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,33 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * ClassName: RedisConfiguration
+ * Package: com.sky.config
+ * Description: This is a program for testing.
+ *
+ * @Author Ben
+ * @Create 2024/12/31 10:07
+ * @Version 1.0
+ */
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建redis模板对象");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
